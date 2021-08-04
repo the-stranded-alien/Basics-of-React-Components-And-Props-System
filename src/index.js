772ff30661d7498f33ff943f15ccadfe6238b31a
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2021-08-04 18:05:59 +0530

    More with Props & Props.Children

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetails';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                author="Sam"
-                timeAgo="Today at 4:00 AM"
-                content="Funny !" 
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Alex"
-                timeAgo="Today at 7:00 AM" 
-                content="Not Funny !" 
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Jane" 
-                timeAgo="Today at 2:00 PM" 
-                content="Funny Indeed !" 
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam"
+                    timeAgo="Today at 4:00 AM"
+                    content="Funny !" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex"
+                    timeAgo="Today at 7:00 AM" 
+                    content="Not Funny !" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Today at 2:00 PM" 
+                    content="Funny Indeed !" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
